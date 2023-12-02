f379d4a0b95cf08da791a667981d1acd092a1954
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-02 19:53:31 +0300

    Navbar 1

--- gym-fitness/src/App.tsx
+++ gym-fitness/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './screen/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,8 @@ function App() {
   return (
     <>
       <div className=''>
-      <h1 className="text-3xl sm:bg-primary-500  text-black font-bold underline">
-      Hello world!
-    </h1>
-    <p className='font-monsterrat'>Test</p>
-    <p className='font-dmssans'>Test</p>
-    <p>Test</p>
+        <Navbar></Navbar>
+     
       </div>
     </>
   )
