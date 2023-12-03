8dc811dae4165702274679c5fc7c694a90bd1ae6
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-03 15:03:27 +0300

    benefit2

--- gym-fitness/src/screen/Benefit/benefit.tsx
+++ gym-fitness/src/screen/Benefit/benefit.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
 
-const benefit = (props: Props) => {
+    icon:JSX.Element;
+    title:string;
+    description:string;
+}
+
+const BenefitDesing = ({icon, title, description}: Props) => {
   return (
-    <div>benefit</div>
+    <div className='mt-5 md:mr-5 rounded-md border-2 text-center px-5 py-16'>
+
+        <div className='mb-4 flex justify-center'>
+
+            <div className=' text-center rounded-full border-2 p-4 border-gray-100 bg-primary-100'>
+
+                {icon}
+            </div>
+        </div>
+
+        <h4 className='font-bold'>{title}</h4>
+        <p className='my-3'>{description}</p>
+
+
+    </div>
   )
 }
 
-export default benefit
+export default BenefitDesing
--- gym-fitness/src/screen/Benefit/index.tsx
+++ gym-fitness/src/screen/Benefit/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Htext from '../../shared/Htext'
 import { BenefitTypes } from '../../shared/types'
-import { HomeIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, UserIcon, PhoneIcon } from '@heroicons/react/24/outline'
+import BenefitDesing from './benefit'
 
 type Props = {}
 
@@ -13,12 +14,12 @@ const benefits:Array<BenefitTypes>=[
     },
     {
         icon:<UserIcon className='h-6 w-6 text-black'/>,
-        title:'What is Lorem Ipsum?',
+        title:'What is2 Lorem Ipsum?',
         description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
     },
     {
         icon:<PhoneIcon className='h-6 w-6 text-black'/>,
-        title:'What is Lorem Ipsum?',
+        title:'What is3 Lorem Ipsum?',
         description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
     },
 
@@ -29,13 +30,35 @@ const Benefits = (props: Props) => {
         
        <div className='md:w-3/5'>
 
-       <Htext>More than just gym</Htext>
+                <Htext>More than just gym</Htext>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore illo autem veniam maxime sint nihil voluptate fuga doloribus distinctio voluptatibus, excepturi, asperiores temporibus officiis iste quos deleniti magni quia error.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore illo autem veniam maxime sint nihil voluptate fuga doloribus distinctio voluptatibus, excepturi, asperiores temporibus officiis iste quos deleniti magni quia error.</p>
+
+
+       </div>
+
+       <div className='md:flex items-center justify-between mt-2'>
+
+        {benefits.map((benefit:BenefitTypes)=>(
+            <BenefitDesing
+            key={benefit.title}
+            icon={benefit.icon}
+            title={benefit.title}
+            description={benefit.description}
+
+            
+            
+            />
+
+           
+
+
+        ))}
 
 
        </div>
     
+
     
     
     </section>
