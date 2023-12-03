16edd5205bf024cdb8559c99f1a69d9db56e476a
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-03 15:47:15 +0300

    benefit animation

--- gym-fitness/src/screen/Benefit/index.tsx
+++ gym-fitness/src/screen/Benefit/index.tsx
@@ -3,6 +3,7 @@ import Htext from '../../shared/Htext'
 import { BenefitTypes } from '../../shared/types'
 import { HomeIcon, UserIcon, PhoneIcon } from '@heroicons/react/24/outline'
 import BenefitDesing from './benefit'
+import {motion} from "framer-motion"
 
 type Props = {}
 
@@ -28,6 +29,16 @@ const Benefits = (props: Props) => {
   return (
     <section id="benefits" className='mx-auto min-h-full w-5/6 py-20'>
         
+        <motion.div
+               initial="hidden"
+               whileInView="visible"
+               viewport={{ once: true, amount: 0.5 }}
+               transition={{ duration: 0.5 }}
+               variants={{
+                 hidden: { opacity: 0, x: -50 },
+                 visible: { opacity: 1, x: 0 },
+               }}
+               >
        <div className='md:w-3/5'>
 
                 <Htext>More than just gym</Htext>
@@ -36,10 +47,21 @@ const Benefits = (props: Props) => {
 
 
        </div>
+       </motion.div>
 
        <div className='md:flex items-center justify-between mt-2'>
 
         {benefits.map((benefit:BenefitTypes)=>(
+
+            <motion.div
+
+            key={benefit.title}
+            initial={{ opacity: 0, y: -20 }} // Initial state
+            animate={{ opacity: 1, y: 0 }}   // Animation state
+            exit={{ opacity: 0, y: -20 }}    // Exit state (for unmounting)
+
+            
+            >
             <BenefitDesing
             key={benefit.title}
             icon={benefit.icon}
@@ -49,7 +71,7 @@ const Benefits = (props: Props) => {
             
             
             />
-
+        </motion.div>
            
 
 
